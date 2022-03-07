fb2411cad47b781ca40955148d4a8457141c2515
Sariputa Tiong <info@digitalfuture.melbourne>
2022-03-07 21:46:08 +1100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = { //Initialise employee object
+    name: "Test Employee",
+    streetAddress: "Test Address"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employees = {...employee}
+    employees.name = "Sam"
+    employees.streetAddress = "11 Broadway"
+    return employees
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam"
+    employee.streetAddress = "12 Broadway"
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employees = {...employee}
+    delete employees[key]
+    return employees
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
